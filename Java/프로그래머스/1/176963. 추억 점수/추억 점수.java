64f21a91037cfd787ba1009023215203e9e35616
kyungminseo <110973127+mxinseo@users.noreply.github.com>
2024-12-22 19:34:34 +0900

    [level 1] Title: 추억 점수, Time: 22.62 ms, Memory: 89.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];   // 사진의 개수만큼 배열 크기 설정
+        
+        // 각 사진을 하나씩 꺼냄
+        for (int i =0; i < photo.length; i++) {
+            int tmp = 0;
+            
+            for (String n : photo[i]) {
+                if (Arrays.asList(name).contains(n)) {  // 사진의 이름이 name에 있는지 확인 
+                    int index = Arrays.asList(name).indexOf(n); // 있으면 index를 찾아서
+                    tmp += yearning[index]; // 해당 점수를 tmp에 더해감
+                }   
+            }
+            
+            answer[i] = tmp;
+        }
+
+        return answer;
+    }
+}
