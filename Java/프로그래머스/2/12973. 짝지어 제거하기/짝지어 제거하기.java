fbd50125159d523f4b7c0e106126322275a513b7
kyungminseo <110973127+mxinseo@users.noreply.github.com>
2024-12-22 20:51:11 +0900

    [level 2] Title: 짝지어 제거하기, Time: 113.67 ms, Memory: 87.1 MB -BaekjoonHub

--- Java/프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ Java/프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -1,5 +1,4 @@
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.Stack;
 
 class Solution
 {
@@ -7,40 +6,29 @@ class Solution
     {
         int answer = -1;
         
-        // 앞쪽의 알파벳 2개부터 비교해가는 선입선출의 구조
-        // s 의 알파벳 각각을 덱으로 변환
-        ArrayDeque<String> queue = new ArrayDeque<>();
-        
-        // 제거 후 앞부분 알파벳들을 저장할 임시 큐
-        ArrayDeque<String> tmp = new ArrayDeque<>();
+        // s안의 알파벳을 이전 알파벳과 바로 다음 알파벳을 비교하므로 
+        // 선입후출의 스택이 적합
+        Stack<Character> tmp = new Stack<>();
         
         for (char c : s.toCharArray()) {
-            queue.addLast(String.valueOf(c));
-        }
-        
-        
-        while(!queue.isEmpty()) {
             
-            // tmp의 맨뒤랑 queue의 맨 앞이 같으면 소거 = peek한 알파벳도 poll 
-            // 단, tmp가 비어있으면 다음 queue의 알파벳을 넣어야 하므로 isEmpty조건 추가
-            if(!tmp.isEmpty() && tmp.peekLast().equals(queue.peekFirst())) {
-                tmp.pollLast();
-                queue.pollFirst();
+            // tmp의 맨 위와 같으면 제거
+            // 단, tmp가 비어있으면 다음 알파벳을 넣어야 하므로 isEmpty조건 추가
+            if(!tmp.isEmpty() && tmp.peek() == c) {
+                tmp.pop();
             } else {
-                // 다르면 queue의 맨 앞 앞파벳을 tmp에 넣어두고 다음 검사 실행
-                tmp.addLast(queue.pollFirst());
-            }
+                // 다르면 tmp에 넣어두고 다음 검사 실행
+                tmp.push(c);
+            }   
         }
-        
+
         if(tmp.isEmpty()) {
-            // queue가, tmp 모두 비어있으면 1 리턴
+            // tmp 모두 비어있으면 1 리턴
             answer = 1;    
         } else {
-            // queue가 비어있는데, tmp가 비어있지 않으면 0 리턴
+            // tmp가 비어있지 않으면 0 리턴
             answer = 0;
         }
-        
-           
 
         return answer;
     }
